61fee93f89b2428b95affa4a055a67332b5af880
Guilherme Correa <correacgui@gmail.com>
2022-05-24 14:31:02 -0300

    Added body-parser, cors and express-session

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const session = require('express-session')
+
 //imported routes
 const usersRouter = require('../routes/user')
 const cartRouter = require('../routes/cart')
 const productsRouter = require('../routes/products')
 
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+app.use(cors())
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+  credentials: true,
+  name: 'sid',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.ENVIRONMENT === 'production',
+    httpOnly: true,
+    sameSite: process.env.ENVIRONMENT ? "none" : "lax",
+  },
+}))
 
 // app.use(routes)
 app.use('/users', usersRouter)
