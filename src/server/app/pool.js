5faf901b89c8d5e0f3562cd0bec3b96c44394d70
Guilherme Correa <correacgui@gmail.com>
2022-05-24 15:48:21 -0300

    Added Procfile.txt

--- src/server/app/pool.js
+++ src/server/app/pool.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
 const Pool = require('pg').Pool
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.BD_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL
+	connectionString: isProduction ? process.env.DATABASE_URL :	connectionString,
+	ssl: {
+        rejectUnauthorized: false,
+    },
 })
 
 module.exports = pool
