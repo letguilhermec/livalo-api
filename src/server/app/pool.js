86bd5fc56d9e8fad561d2ba1ea64cbb5b1294858
Guilherme Correa <correacgui@gmail.com>
2022-05-24 15:32:48 -0300

    Changed .env to accomodate Heroku

--- src/server/app/pool.js
+++ src/server/app/pool.js
@@ -2,11 +2,7 @@ require('dotenv').config()
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT
+	connectionString: process.env.DATABASE_URL
 })
 
 module.exports = pool
