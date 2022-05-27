0bd4bdaadcde3ec5108630eff12e2415a638de9e
Guilherme Correa <correacgui@gmail.com>
2022-05-27 17:38:29 -0300

    Added a route to get all products

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -4,14 +4,19 @@ const prodModel = require('../utils/models/productsModel')
 const ProductsModel = new prodModel('prods')
 
 productsRouter.get('/show/:offset', async (req, res) => {
-	let offset = Number(req.params.offset)
-	const products = await ProductsModel.getProducts(offset)
-	return res.status(200).json(products.rows)
+  let offset = Number(req.params.offset)
+  const products = await ProductsModel.getProducts(offset)
+  return res.status(200).json(products.rows)
 })
 
 productsRouter.get('/pages', async (req, res) => {
-	const NumOfPages = await ProductsModel.getPageTotal()
-	return res.status(200).json(NumOfPages)
+  const NumOfPages = await ProductsModel.getPageTotal()
+  return res.status(200).json(NumOfPages)
+})
+
+productsRouter.get('/all', async (req, res) => {
+  const products = await ProductsModel.getTotalProds()
+  return res.status(200).json(products.rows)
 })
 
 module.exports = productsRouter
--- src/server/utils/models/productsModel.js
+++ src/server/utils/models/productsModel.js
@@ -1,32 +1,36 @@
 const pool = require('../../app/pool')
 
 class Model {
-	constructor(table) {
-		this.table = table
-		this.pool = pool
-		this.pool.on('error', (err, client) => `Error ${err} on client ${client}`)
-	}
+  constructor(table) {
+    this.table = table
+    this.pool = pool
+    this.pool.on('error', (err, client) => `Error ${err} on client ${client}`)
+  }
 
-	async getTotalProducts() {
-		return this.pool.query(`SElECT COUNT(*) FROM ${this.table}`)
-	}
+  async getTotalProducts() {
+    return this.pool.query(`SElECT COUNT(*) FROM ${this.table}`)
+  }
 
-	async getProducts(offset) {
-		if (parseInt(offset) === 1) {
-			offset = 0
-		} else {
-			offset = parseInt(offset - 1) * 12
-		}
-		return this.pool.query(
-			`SELECT * FROM ${this.table} LIMIT 12 OFFSET ${offset}`
-		)
-	}
+  async getProducts(offset) {
+    if (parseInt(offset) === 1) {
+      offset = 0
+    } else {
+      offset = parseInt(offset - 1) * 12
+    }
+    return this.pool.query(
+      `SELECT * FROM ${this.table} LIMIT 12 OFFSET ${offset}`
+    )
+  }
 
-	async getPageTotal() {
-		const totalProds = await this.getTotalProducts()
-		const totalPagesNum = Math.ceil(Number(totalProds.rows[0].count) / 12)
-		return totalPagesNum
-	}
+  async getPageTotal() {
+    const totalProds = await this.getTotalProducts()
+    const totalPagesNum = Math.ceil(Number(totalProds.rows[0].count) / 12)
+    return totalPagesNum
+  }
+
+  async getTotalProds() {
+    return this.pool.query(`SELECT * FROM ${this.table}`)
+  }
 }
 
 module.exports = Model
