69ff96ce9302ce8c4c92f25cac97b17123f0d907
Guilherme Correa <correacgui@gmail.com>
2022-06-15 13:34:13 -0300

    Added missing await to some /cart routes

--- src/server/routes/cart.js
+++ src/server/routes/cart.js
@@ -8,122 +8,122 @@ const tempModel = require('../utils/models/TemporaryCartModel')
 const TemporaryCart = new tempModel('temp_cart')
 
 cartRouter.post('/getcart', checkCart, async (req, res) => {
-	let cart
-	const status = req.cartStatus
+  let cart
+  const status = req.cartStatus
 
-	if (status === 'Permanent') {
-		// cart = await PermanentCart.getCartByNum(req.cartNum)
-		cart = await PermanentCart.getFromCart(req.cartNum)
-		return res.status(200).json(cart.rows)
-	}
+  if (status === 'Permanent') {
+    // cart = await PermanentCart.getCartByNum(req.cartNum)
+    cart = await PermanentCart.getFromCart(req.cartNum)
+    return res.status(200).json(cart.rows)
+  }
 
-	if (status === 'Temporary') {
-		// cart = await TemporaryCart.getCartByNum(req.cartNum)
-		cart = await TemporaryCart.getFromCart(req.cartNum)
-		return res.status(200).json(cart.rows)
-	}
+  if (status === 'Temporary') {
+    // cart = await TemporaryCart.getCartByNum(req.cartNum)
+    cart = await TemporaryCart.getFromCart(req.cartNum)
+    return res.status(200).json(cart.rows)
+  }
 
-	return res.status(400).json('No cart number informed')
+  return res.status(400).json('No cart number informed')
 })
 
 cartRouter.post('/add', checkCart, async (req, res) => {
-	const status = req.cartStatus
-	const cartNum = req.cartNum
-	const { prodId } = req.body
+  const status = req.cartStatus
+  const cartNum = req.cartNum
+  const { prodId } = req.body
 
-	if (!prodId) {
-		return res.status(400).json('No selected product')
-	}
+  if (!prodId) {
+    return res.status(400).json('No selected product')
+  }
 
-	if (status === 'Permanent') {
-		const checkExists = await PermanentCart.checkProduct(cartNum, prodId)
-		if (checkExists.rows.length === 0) {
-			const inserted = PermanentCart.addToCart(cartNum, prodId)
-			return res.status(200).json(inserted.rows)
-		} else {
-			const added = PermanentCart.addQuantity(cartNum, prodId)
-			return res.status(200).json(added.rows)
-		}
-	}
+  if (status === 'Permanent') {
+    const checkExists = await PermanentCart.checkProduct(cartNum, prodId)
+    if (checkExists.rows.length === 0) {
+      const inserted = await PermanentCart.addToCart(cartNum, prodId)
+      return res.status(200).json(inserted.rows)
+    } else {
+      const added = await PermanentCart.addQuantity(cartNum, prodId)
+      return res.status(200).json(added.rows)
+    }
+  }
 
-	if (status === 'Temporary') {
-		const checkExists = await TemporaryCart.checkProduct(cartNum, prodId)
-		if (checkExists.rows.length === 0) {
-			const inserted = TemporaryCart.addToCart(cartNum, prodId)
-			return res.status(200).json(inserted.rows)
-		} else {
-			const added = TemporaryCart.addQuantity(cartNum, prodId)
-			return res.status(200).json(added.rows)
-		}
-	}
+  if (status === 'Temporary') {
+    const checkExists = await TemporaryCart.checkProduct(cartNum, prodId)
+    if (checkExists.rows.length === 0) {
+      const inserted = await TemporaryCart.addToCart(cartNum, prodId)
+      return res.status(200).json(inserted.rows)
+    } else {
+      const added = await TemporaryCart.addQuantity(cartNum, prodId)
+      return res.status(200).json(added.rows)
+    }
+  }
 
-	if (status === 'None') {
-		const createdCart = await TemporaryCart.createTempCart(prodId)
-		const cart = await TemporaryCart.getCartByNum(createdCart.rows[0].user_cart)
-		return res
-			.status(200)
-			.json({ cartNum: cart.rows[0].user_cart, cart: cart.rows })
-	}
+  if (status === 'None') {
+    const createdCart = await TemporaryCart.createTempCart(prodId)
+    const cart = await TemporaryCart.getCartByNum(createdCart.rows[0].user_cart)
+    return res
+      .status(200)
+      .json({ cartNum: cart.rows[0].user_cart, cart: cart.rows })
+  }
 })
 
 cartRouter.put('/sub', checkCart, async (req, res) => {
-	const { prodId } = req.body
-	const status = req.cartStatus
-	const cartNum = req.cartNum
+  const { prodId } = req.body
+  const status = req.cartStatus
+  const cartNum = req.cartNum
 
-	if (!prodId) {
-		return res.status(400).json('No selected product')
-	}
+  if (!prodId) {
+    return res.status(400).json('No selected product')
+  }
 
-	if (status === 'None') {
-		return res.status(400).json('No cart number informed')
-	}
+  if (status === 'None') {
+    return res.status(400).json('No cart number informed')
+  }
 
-	if (status === 'Permanent') {
-		const subItem = await PermanentCart.subQuantity(cartNum, prodId)
-		if (subItem.rows.length === 0) {
-			return res.status(400).json('Action could not be performed')
-		}
-		return res.status(200).json(subItem.rows)
-	}
+  if (status === 'Permanent') {
+    const subItem = await PermanentCart.subQuantity(cartNum, prodId)
+    if (subItem.rows.length === 0) {
+      return res.status(400).json('Action could not be performed')
+    }
+    return res.status(200).json(subItem.rows)
+  }
 
-	if (status === 'Temporary') {
-		const subItem = await TemporaryCart.subQuantity(cartNum, prodId)
-		if (subItem.rows.length === 0) {
-			return res.status(400).json(subItem.rows)
-		}
-		return res.status(200).json(subItem.rows)
-	}
+  if (status === 'Temporary') {
+    const subItem = await TemporaryCart.subQuantity(cartNum, prodId)
+    if (subItem.rows.length === 0) {
+      return res.status(400).json('Action could not be performed')
+    }
+    return res.status(200).json(subItem.rows)
+  }
 })
 
 cartRouter.delete('/del', checkCart, async (req, res) => {
-	const {prodId} = req.body
-	const status = req.cartStatus
-	const cartNum = req.cartNum
+  const { prodId } = req.body
+  const status = req.cartStatus
+  const cartNum = req.cartNum
 
-	if (!prodId) {
-		return res.status(400).json('No selected product')
-	}
+  if (!prodId) {
+    return res.status(400).json('No selected product')
+  }
 
-	if (status === 'None') {
-		return res.status(400).json('No cart number informed')
-	}
+  if (status === 'None') {
+    return res.status(400).json('No cart number informed')
+  }
 
-	if (status === 'Permanent') {
-		const delItem = await PermanentCart.deleteFromCart(cartNum, prodId)
-		if (delItem.rows.length === 0) {
-			return res.status(400).json('Product not found on cart')
-		}
-		return res.status(200).json(delItem.rows)
-	}
+  if (status === 'Permanent') {
+    const delItem = await PermanentCart.deleteFromCart(cartNum, prodId)
+    if (delItem.rows.length === 0) {
+      return res.status(400).json('Product not found on cart')
+    }
+    return res.status(200).json(delItem.rows)
+  }
 
-	if (status === "Temporary") {
-		const delItem = await TemporaryCart.deleteFromCart(cartNum, prodId)
-		if (delItem.rows.length === 0) {
-			return res.status(400).json('Product not found on cart')
-		}
-		return res.status(200).json(delItem.rows)
-	}
+  if (status === "Temporary") {
+    const delItem = await TemporaryCart.deleteFromCart(cartNum, prodId)
+    if (delItem.rows.length === 0) {
+      return res.status(400).json('Product not found on cart')
+    }
+    return res.status(200).json(delItem.rows)
+  }
 })
 
 module.exports = cartRouter
--- src/server/utils/models/TemporaryCartModel.js
+++ src/server/utils/models/TemporaryCartModel.js
@@ -1,75 +1,75 @@
 const pool = require('../../app/pool')
 
 class Model {
-	constructor(table) {
-		this.table = table
-		this.pool = pool
-		this.pool.on('error', (err, client) => `Error ${err} on client ${client}`)
-	}
+  constructor(table) {
+    this.table = table
+    this.pool = pool
+    this.pool.on('error', (err, client) => `Error ${err} on client ${client}`)
+  }
 
-	async createTempCart(prodId) {
-		return this.pool.query(
-			`INSERT INTO ${this.table} (prod_id) VALUES ('${prodId}') RETURNING user_cart`
-		)
-	}
+  async createTempCart(prodId) {
+    return this.pool.query(
+      `INSERT INTO ${this.table} (prod_id) VALUES ('${prodId}') RETURNING user_cart`
+    )
+  }
 
-	async deleteTempCart(cartNum) {
-		return this.pool.query(
-			`DELETE FROM ${this.table} WHERE user_cart = '${cartNum}' RETURNING *`
-		)
-	}
+  async deleteTempCart(cartNum) {
+    return this.pool.query(
+      `DELETE FROM ${this.table} WHERE user_cart = '${cartNum}' RETURNING *`
+    )
+  }
 
-	async getCartByNum(cartNum) {
-		return this.pool.query(
-			`SELECT * FROM ${this.table} WHERE user_cart = '${cartNum}'`
-		)
-	}
+  async getCartByNum(cartNum) {
+    return this.pool.query(
+      `SELECT * FROM ${this.table} WHERE user_cart = '${cartNum}'`
+    )
+  }
 
-	async checkProduct(cartNum, prodId) {
-		return this.pool.query(
-			`SELECT * FROM ${this.table} WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}'`
-		)
-	}
+  async checkProduct(cartNum, prodId) {
+    return this.pool.query(
+      `SELECT * FROM ${this.table} WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}'`
+    )
+  }
 
-	async getFromCart(cartNum) {
-		return this.pool.query(
-			`SELECT prods.id, prods.image, prods.name, prods.brand, prods.price, ${this.table}.quantity FROM prods JOIN ${this.table} ON prods.id = ${this.table}.prod_id AND ${this.table}.user_cart = '${cartNum}'`
-		)
-	}
+  async getFromCart(cartNum) {
+    return this.pool.query(
+      `SELECT prods.id, prods.image, prods.name, prods.brand, prods.price, ${this.table}.quantity FROM prods JOIN ${this.table} ON prods.id = ${this.table}.prod_id AND ${this.table}.user_cart = '${cartNum}'`
+    )
+  }
 
-	async getProdsFromCart(cartNum) {
-		return this.pool.query(
-			`SELECT prod_id, quantity FROM ${this.table} WHERE user_cart = '${cartNum}'`
-		)
-	}
+  async getProdsFromCart(cartNum) {
+    return this.pool.query(
+      `SELECT prod_id, quantity FROM ${this.table} WHERE user_cart = '${cartNum}'`
+    )
+  }
 
-	async addToCart(cartNum, prodId) {
-		return this.pool.query(
-			`INSERT INTO ${this.table} (user_cart, prod_id) VALUES ('${cartNum}', '${prodId}') RETURNING *`
-		)
-	}
+  async addToCart(cartNum, prodId) {
+    return this.pool.query(
+      `INSERT INTO ${this.table} (user_cart, prod_id) VALUES ('${cartNum}', '${prodId}') RETURNING *`
+    )
+  }
 
-	async deleteFromCart(cartNum, prodId) {
-		return this.pool.query(
-			`DELETE FROM ${this.table} WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}' RETURNING prod_id`
-		)
-	}
+  async deleteFromCart(cartNum, prodId) {
+    return this.pool.query(
+      `DELETE FROM ${this.table} WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}' RETURNING prod_id`
+    )
+  }
 
-	async addQuantity(cartNum, prodId) {
-		return this.pool.query(
-			`UPDATE ${this.table} SET quantity = quantity + 1 WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}' RETURNING *`
-		)
-	}
+  async addQuantity(cartNum, prodId) {
+    return this.pool.query(
+      `UPDATE ${this.table} SET quantity = quantity + 1 WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}' RETURNING *`
+    )
+  }
 
-	async subQuantity(cartNum, prodId) {
-		return this.pool.query(
-			`UPDATE ${this.table} SET quantity = quantity - 1 WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}' RETURNING *`
-		)
-	}
+  async subQuantity(cartNum, prodId) {
+    return this.pool.query(
+      `UPDATE ${this.table} SET quantity = quantity - 1 WHERE user_cart = '${cartNum}' AND prod_id = '${prodId}' RETURNING *`
+    )
+  }
 
-	async truncate() {
-		return this.pool.query(`TRUNCATE ${this.table} CASCADE`)
-	}
+  async truncate() {
+    return this.pool.query(`TRUNCATE ${this.table} CASCADE`)
+  }
 }
 
 module.exports = Model
