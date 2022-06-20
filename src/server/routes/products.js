b287983105c6340436466a12bbf81038c9157442
Guilherme Correa <correacgui@gmail.com>
2022-06-20 16:56:02 -0300

    Added /products/:id

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -2,6 +2,8 @@ const productsRouter = require('express').Router()
 
 const prodModel = require('../utils/models/productsModel')
 const ProductsModel = new prodModel('prods')
+const prodsInfoModel = require('../utils/models/prodsInfoModel')
+const ProdsInfoModel = new prodsInfoModel('prods_info')
 
 productsRouter.get('/show/:offset', async (req, res) => {
   let offset = Number(req.params.offset)
@@ -17,6 +19,10 @@ productsRouter.get('/pages', async (req, res) => {
 productsRouter.get('/all', async (req, res) => {
   const products = await ProductsModel.getTotalProds()
   return res.status(200).json(products.rows)
+})
+
+productsRouter.get('/product/:id', async (req, res) => {
+  return res.status(200).json(req.params.id)
 })
 
 module.exports = productsRouter
